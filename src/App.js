b914a33576ccf2d9356c189d8c804b374625ca03
jmachinadev <jmachinadev@gmail.com>
2021-03-03 19:32:06 -0500

    create component and css files ready to be filled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './css/App.css';
 import DisplayInfo from './components/displayInfo';
+
 
 function App() {
   return (
---
+++ src/components/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import 'Home.js';
+
+const Home = () => {
+  return <></>;
+}
+
+export default Home;
