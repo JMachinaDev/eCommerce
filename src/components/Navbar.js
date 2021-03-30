6f25c9709992d9aaa5c0ff39698ba679c2a9d0fa
jmachinadev <jmachinadev@gmail.com>
2021-03-30 12:31:55 -0400

    add nav bar to application, with live links to api for signin and signup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import 'Navbar.js';
+import { Link, Route } from 'react-router-dom';
 
 const Navbar = () => {
-  return <></>;
+  return (
+    <main>
+      <nav>
+        <div>
+          {/* brand name */}
+          <a>El Supremo</a>
+        </div>
+        <div>
+          <li>
+            <Link to={'/signup'}>Sign Up</Link>
+          </li>
+          <li>
+            <Link to={'/login'}>Login</Link>
+          </li>
+        </div>
+      </nav>
+
+      <div className="crud-container">
+        <Switch>
+          <Route exact path={['/', '/signup']} component={UserList} />
+          <Route exact path='/add' component={AddUser} />
+          <Route exact path='/users/:id' component={Users} />
+        </Switch>
+      </div>
+    </main>
+  )
 }
 
 export default Navbar;
