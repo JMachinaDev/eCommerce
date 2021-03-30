cd7f6e3a987ee6256f88aa106aa6ef804ded6e6d
jmachinadev <jmachinadev@gmail.com>
2021-03-30 12:31:07 -0400

    minor formatting , and removed unecessary files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+
+import "react-bootstrap";
 import './css/App.css';
 import DisplayInfo from './components/displayInfo';
 
-
 function App() {
   return (
-    <>
-      <div className="App"><DisplayInfo /></div>
-    </>
+    <div className="App">
+      <DisplayInfo />
+    </div>
   );
 }
 
