74079386581226e3368bbb95899d5a99c6aae32d
jmachinadev <jmachinadev@gmail.com>
2021-04-15 15:59:42 -0400

    **WIP** add Admin and Moderator components

---
+++ src/components/BoardAdmin.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+// import UserService from '../services/user.service';
+
+const BoardAdmin = () => {
+  console.log('board moderator empty');
+  return (
+    <div>empty</div>
+  )
+};
+
+export default BoardAdmin;
