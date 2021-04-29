f5518e4ab3c348acaa4ffa071cf5c720cabe8c3e
jmachinadev <jmachinadev@gmail.com>
2021-04-29 13:36:46 -0400

    correct script file name

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/aws-codedeploy
 pm2 delete all
-pm2 startOrReload ecosystem.js
+pm2 startOrReload ecosystem.config.js
 pm2 save
 pm2 startup
 # pm2 startOrReload ecosystem.config.js 
