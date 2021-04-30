f05aceba55980b1557b82af5768ef74e0e316b44
jmachinadev <jmachinadev@gmail.com>
2021-04-30 13:12:08 -0400

    rever pm2 changes

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 cd ~/aws-codedeploy
-pm2 delete all
 pm2 startOrReload ecosystem.config.js
-pm2 save
-pm2 startup
-=======
-#!/bin/bash
-cd ~/aws-codedeploy
-pm2 delete all
-pm2 startOrReload ecosystem.config.js
-pm2 save
-pm2 startup
->>>>>>> origin/staging
-# pm2 startOrReload ecosystem.config.js 
