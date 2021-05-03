57763d7bc968c1318ed2cd199580cf28457a746e
jmachinadev <jmachinadev@gmail.com>
2021-05-02 21:31:54 -0400

    change to port 3000, corrected pm2 script

--- scripts/reload-pm2.sh
+++ scripts/reload-pm2.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cd ~/aws-codedeploy
-pm2 delete all
-pm2 startOrReload ecosystem.js
-pm2 save
-pm2 startup
-# pm2 startOrReload ecosystem.config.js 
+pm2 startOrReload ecosystem.config.js
