0ce5b6907a26b174b5e27402144d7ef9c513a70f
jmachinadev <jmachinadev@gmail.com>
2021-05-04 11:23:06 -0400

    remove unecessary files

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'aws-codedeploy',
+      name: 'frontend',
       script: 'npx',
       args: 'serve -s build -l 3000 -n',
       interpreter: 'none',
