01f047ba68bd236d4187164799603567e9f6c73d
jmachinadev <jmachinadev@gmail.com>
2021-05-12 16:06:09 -0400

    created .env develpment and production files. pass correct arguments

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_DEV : process.env.REACT_APP_DEV;
+const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION : process.env.REACT_APP_DEVELOPMENT;
 
 export default axios.create({
   baseURL: apiUrl,
