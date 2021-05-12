63c4275b7bb19a95bca96ae697aefbb19586c3d4
jmachinadev <jmachinadev@gmail.com>
2021-05-12 16:08:06 -0400

    ignore .env files

--- src/http-common.js
+++ src/http-common.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const apiUrl = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_PRODUCTION : process.env.REACT_APP_DEVELOPMENT;
 
 export default axios.create({
-  baseURL: apiUrl,
+  baseURL: 'http://54.91.248.172:8081/',
   headers: {
     "Content-type": "application/json"
   }
